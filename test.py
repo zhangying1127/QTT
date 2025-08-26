813f68dfa47ec21b4239131cdde6858c045941d3
zhangying1127 <zying@gdpu.edu.cn>
2025-08-26 22:23:17 +0800

    123

--- test.py
+++ test.py
@@ -7,8 +7,5 @@ print(plt.rcParams['font.sans-serif'])  # 确认字体设置
 plt.plot([1,2], [3,4])
 plt.title('中文测试')
 print("test.....")
-print("test.....")
-print("test.....")
-print("test.....")
-print("test.....")
+
 plt.show()
